67d1273c5697b2ae51f3ab50c0bf5ca03920c000
varshitha1707 <varshithasreddy@gmail.com>
2021-10-13 00:35:14 +0530

    faqs added

---
+++ src/Pages/Homepage/Components/FAQs/FAQs.js
@@ -0,0 +1,100 @@
+import * as React from 'react';
+import Accordion from '@mui/material/Accordion';
+// import AccordionDetails from '@mui/material/AccordionDetails';
+// import AccordionSummary from '@mui/material/AccordionSummary';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { Container } from '@material-ui/core';
+import { styled } from '@mui/material/styles';
+import MuiAccordionSummary from '@mui/material/AccordionSummary';
+import MuiAccordionDetails from '@mui/material/AccordionDetails';
+import './FAQs.css'
+
+  
+  const AccordionSummary = styled((props) => (
+    <MuiAccordionSummary
+    // expandIcon={<ExpandMoreIcon />}
+      {...props}
+    />
+  ))(({ theme }) => ({
+    //   alternate colours for the accordion implementation
+    backgroundColor: '#F3F6F9',
+    flexDirection: 'row',
+    '& .MuiAccordionSummary-content': {
+      margin: theme.spacing(1),
+      textAlign: 'center',
+    //   align the text to center
+    },
+  }));
+  
+  const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
+    padding: theme.spacing(2),
+    borderTop: '1px solid rgba(0, 0, 0, .125)',
+  }));
+
+export default function ControlledAccordions() {
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleChange = (panel) => (event, isExpanded) => {
+    setExpanded(isExpanded ? panel : false);
+  };
+
+  return (
+    <Container id="FAQs-container">
+        <h1 id="FAQs-title">FAQ'S</h1>
+      {
+        FAQs.map((faq)=>{
+          return(
+            <Accordion key={faq.id} disableGutters elevation={0} id="Accordion" expanded={expanded === faq.panel} onChange={handleChange(faq.panel)}>
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon sx={{right:"0"}}/>}
+                id="AccordionSummary" width="100%"
+              >
+                <div id="FAQs-acc-title">
+                    <h3>{faq.title}</h3>
+                </div>
+              </AccordionSummary>
+              <AccordionDetails >
+                <h4 id="FAQs-acc-text">{faq.text}</h4>
+              </AccordionDetails>
+            </Accordion>
+          )
+        })
+      }
+    </Container>
+  );
+}
+
+
+
+const FAQs =[
+    {
+        id:1,
+        title:"What is average salary per month working as tutor here?",
+        text:"Something Something Something",
+        panel:'panel1',
+    },
+    {
+        id:2,
+        title:"How many hours the work can be ?",
+        text:"Something Something Something",
+        panel:'panel2',
+    },
+    {
+        id:3,
+        title:"Are the working hours flexible?",
+        text:"Something Something Something",
+        panel:'panel3',
+    },
+    {
+        id:4,
+        title:"Does the solutions needs to be plagirism free?",
+        text:"Something Something Something",
+        panel:'panel4',
+    },
+    {
+        id:5,
+        title:"Can we leave the work anytime if needed?",
+        text:"Something Something Something",
+        panel:'panel5',
+    }
+  ]
--- src/Pages/Homepage/Home.js
+++ src/Pages/Homepage/Home.js
@@ -4,6 +4,7 @@ import Hero from './Components/Hero/Hero'
 import Slider from '../Components/slider'
 import Benefits from './Components/Benefits/Benefits'
 import Tow from './Components/TypesofWork/ToW'
+import FAQs from './Components/FAQs/FAQs'
 
 function Home() {
     return (
@@ -13,6 +14,7 @@ function Home() {
             {/* <Slider/> */}
             <Benefits/>
             <Tow/>
+            <FAQs/>
         </div>
     )
 }
